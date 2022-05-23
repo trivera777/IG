c254a2545e919814ab4735bf925f31304e065299
Tifanny Rivera <trivera51580@gmail.com>
2022-05-23 13:41:44 -0600

    working navbar component

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useNavbarStyles } from "../../styles";
+import { AppBar } from '@material-ui/core'
+import classes from "*.module.css";
 
 function Navbar() {
   useNavbarStyles();
 
-  return <div>Navbar</div>;
+  return (
+    <AppBar className = {classes.appBar} >
+      <section className={classes.section} >
+        <Logo />
+      </section>
+      </AppBar>
+  )
 }
 
 export default Navbar;
--- src/components/shared/Seo.js
+++ src/components/shared/Seo.js
@@ -5,7 +5,7 @@ function SEO({ title }) {
   const titleText = title ? `${title} ⋅ Instagram` : "Instagram"
   return (
     <Helmet>
-      <title>Title</title>
+      <title>{titleText}</title>
     </Helmet>
   )
 }
