554a40f1308b5e084eac6f48756221f4afac9595
Tifanny Rivera <trivera51580@gmail.com>
2022-05-23 18:22:50 -0600

    building login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,51 @@
 import React from "react";
 import { useLoginPageStyles } from "../styles";
+import SEO from '../components/shared/Seo'
+import { CardHeader, TextField, Card, Button } from "@material-ui/core";
 
 function LoginPage() {
-  useLoginPageStyles();
+ const classes = useLoginPageStyles();
 
-  return <div>LoginPage</div>;
+  return (
+    <>
+    <SEO title="Login" />
+    <section className={classes.section}>
+      <article>
+        <Card className={classes.card}>
+          <CardHeader className={classes.cardHeader} >
+            <form>
+              <TextField 
+              fullWidth
+              variant="filled"
+              label="Username"
+              margin="dense"
+              className={classes.textField}
+              autoComplete="username"
+              />
+               <TextField 
+              fullWidth
+              variant="filled"
+              label="Password"
+              margin="dense"
+              className={classes.textField}
+              autoComplete="current-password"
+              />
+              <Button
+              variant="contained"
+              fullWidth
+              color="primary"
+              className={classes.button}
+              type="submit"
+              >
+              </Button>
+            </form>
+            
+          </CardHeader>
+        </Card>
+      </article>
+    </section>
+    </>
+  )
 }
 
 export default LoginPage;
