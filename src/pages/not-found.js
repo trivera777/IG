d34348db516ccfd9e282a417af5f01242aa07fca
Tifanny Rivera <trivera51580@gmail.com>
2022-05-23 18:08:15 -0600

    not found page done

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,9 +1,24 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
-import Layout from '../components/shared/Layout'
+import Layout from "../components/shared/Layout";
+import {Link} from "react-router-dom";
 
 function NotFoundPage() {
-  return <Layout title="Page Not Found" >NotFoundPage</Layout>;
+  return (
+    <Layout title="Page Not Found" marginTop={120}>
+      <Typography variant="h5" align="center" paragraph>
+        Sorry, this page isn't available.
+      </Typography>
+      <Typography align="center">
+        The link you followed may be broken, or the pahe may have been removed.
+      <Link to="/"> {" "}
+        <Typography color="primary" component="span">
+          Go back to Instagram.
+        </Typography>
+      </Link>
+      </Typography>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
- 
