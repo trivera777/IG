6019cf09a97d156daecccab3d0df1dad18044288
Tifanny Rivera <trivera51580@gmail.com>
2022-06-06 17:08:44 -0600

    inputting dummy data

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
+import FeedPost from '../components/feed/FeedPost'
 import Layout from '../components/shared/Layout'
+import { defaultPost } from '../data'
 
 function FeedPage() {
   const classes = useFeedPageStyles();
 
-  return <Layout>FeedPage</Layout>;
+  return <Layout>
+    <div className={classes.container}>
+      <div>
+        {Array.from({ length: 5 }, () => defaultPost).map(post => (
+          <FeedPost />
+        ))}
+      </div>
+    </div>
+  </Layout>;
 }
 
 export default FeedPage;
