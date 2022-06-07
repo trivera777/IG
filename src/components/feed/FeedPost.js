2691041d6179cdb177369dab8294b13077eebb51
Tifanny Rivera <trivera51580@gmail.com>
2022-06-07 07:53:07 -0600

    adding feed buttons.. like, comment, share

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { MoreIcon } from '../../icons'
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const { media } = post
 
-  return <div>FeedPost</div>;
-}
+  return (
+    <>
+    <article className={classes.article}>
+      <div className={classes.postHeader}>
+        <UserCard />
+        <MoreIcon className={classes.moreIcon} />
+      </div>
+      <div>
+        <img src={media} alt="Post Media" className={classes.image} />
+      </div>
+      <div className={classes.postButtonsWrapper} >
+        <div className={classes.postButtons}>
+
+        </div>
+      </div>
+
+      </article>
+    </>
+  )
+  }
 
 export default FeedPost;
