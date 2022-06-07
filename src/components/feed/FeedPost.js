8c0a71a24e66dd81482a01523ca2f322b776198e
Tifanny Rivera <trivera51580@gmail.com>
2022-06-07 08:20:04 -0600

    creating link to user's profile

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,47 +1,53 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
 import UserCard from "../shared/UserCard";
-import { MoreIcon, CommentIcon, ShareIcon } from '../../icons'
-import { Link } from 'react-router-dom'
+import { MoreIcon, CommentIcon, ShareIcon } from "../../icons";
+import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
-import HTMLEllipsis from 'react-lines-ellipsis/lib/html'
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
 function FeedPost({ post }) {
   const classes = useFeedPostStyles();
-  const { media, id, likes } = post
-  const [showCaption, setCaption] = React.useState(false)
+  const { media, id, likes } = post;
+  const [showCaption, setCaption] = React.useState(false);
 
   return (
     <>
-    <article className={classes.article}>
-      <div className={classes.postHeader}>
-        <UserCard />
-        <MoreIcon className={classes.moreIcon} />
-      </div>
-      <div>
-        <img src={media} alt="Post Media" className={classes.image} />
-      </div>
-      <div className={classes.postButtonsWrapper} >
-        <div className={classes.postButtons}>
-          <LikeButton />
-          <Link to={`/p/${id}`}>
-            <CommentIcon />
+      <article className={classes.article}>
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        <div>
+          <img src={media} alt="Post Media" className={classes.image} />
+        </div>
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
             </Link>
             <ShareIcon />
             <SaveButton />
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+          </div>
         </div>
-        <Typography className={classes.like} variant="subtitle2">
-        <span>{likes === 1 ? '1 like' : `${likes} likes`}</span>
-        </Typography>
-        <div className={showCaption ? classes.expanded : classes.collapsed}>
-          <Link to={`/${user.username}`}>
-            
-          </Link>
-        </div>
-      </div>
       </article>
     </>
-  )
-  }
+  );
+}
 
 export default FeedPost;
