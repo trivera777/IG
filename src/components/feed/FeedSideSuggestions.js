910bd207646f2188c245ecff56e20d01bdf63443
Tifanny Rivera <trivera51580@gmail.com>
2022-06-08 13:23:09 -0600

    adding feed side suggestions component

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,10 +1,27 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import { useFeedSideSuggestionsStyles } from "../../styles";
+import { Paper } from "@material-ui/core";
 
 function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
+  const classes =  useFeedSideSuggestionsStyles();
 
-  return <div>FeedSideSuggestions</div>;
+  return (
+    <article className={classes.article}>
+      <Paper className={classes.paper}>
+        <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        component="h2"
+        align="left"
+        gutterBottom
+        className={classes.typography}
+        >
+          Suggestions For You
+        </Typography>
+      </Paper>
+    </article>
+  )
 }
 
 export default FeedSideSuggestions;
