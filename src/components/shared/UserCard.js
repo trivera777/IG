7c8a104df49f3216354bf551cc9e8137e07fce69
Tifanny Rivera <trivera51580@gmail.com>
2022-06-08 08:18:14 -0600

    UserCard component done

--- src/components/shared/UserCard.js
+++ src/components/shared/UserCard.js
@@ -1,22 +1,38 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, Typography } from "@material-ui/core";
 import React from "react";
 import { useUserCardStyles } from "../../styles";
+import { Link } from "react-router-dom";
+
 
 function UserCard({ user }) {
   const classes = useUserCardStyles();
-  const { username } = user 
+  const { username, name, profile_image } = user;
 
   return (
-    <div className={classes.wrapper} >
+    <div className={classes.wrapper}>
       <Link to={`/${username}`}>
-      <Avatar 
-        src={profile_image}
-        alt="User avatar"
-        className={classes.avatar}
-      />
+        <Avatar
+          src={profile_image}
+          alt="User avatar"
+          className={classes.avatar}
+        />
       </Link>
+      <div className={classes.nameWrapper}>
+        <Link to={`/${username}`}>
+          <Typography variant="subtitle2" className={classes.typography}>
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.typography}
+        >
+          {name}
+        </Typography>
+      </div>
     </div>
-  )
+  );
 }
 
 export default UserCard;
