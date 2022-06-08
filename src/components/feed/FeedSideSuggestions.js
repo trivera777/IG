7988be5ff540b29b2ee1432a5882e7f988f3af09
Tifanny Rivera <trivera51580@gmail.com>
2022-06-08 13:32:41 -0600

    imported follow button

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,27 +1,36 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
 import { useFeedSideSuggestionsStyles } from "../../styles";
-import { Paper } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
+import { getDefaultUser } from '../../data'
+import UserCard from "../shared/UserCard";
+import FollowButtom from '../shared/FollowButton'
+
 
 function FeedSideSuggestions() {
-  const classes =  useFeedSideSuggestionsStyles();
+  const classes = useFeedSideSuggestionsStyles();
 
   return (
     <article className={classes.article}>
       <Paper className={classes.paper}>
         <Typography
-        color="textSecondary"
-        variant="subtitle2"
-        component="h2"
-        align="left"
-        gutterBottom
-        className={classes.typography}
+          color="textSecondary"
+          variant="subtitle2"
+          component="h2"
+          align="left"
+          gutterBottom
+          className={classes.typography}
         >
           Suggestions For You
         </Typography>
+        {Array.from({ length: 5 }, () => getDefaultUser()).map((user) => (
+          <div key={user.id} className={classes.card}>
+            <UserCard user={user} />
+            <FollowButtom />
+          </div>
+        ))}
       </Paper>
     </article>
-  )
+  );
 }
 
 export default FeedSideSuggestions;
